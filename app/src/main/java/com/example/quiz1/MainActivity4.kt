15a5c06ea86f6cfa53342a03e0f31930cdc4ec73
luroose <636051200195@mail.rmutk.ac.th>
2022-03-23 16:43:41 +0700

    กดเกดเ

--- app/src/main/java/com/example/quiz1/MainActivity4.kt
+++ app/src/main/java/com/example/quiz1/MainActivity4.kt
@@ -6,16 +6,17 @@ import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity3 : AppCompatActivity() {
+class MainActivity4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main3)
+        setContentView(R.layout.activity_main4)
         button.setOnClickListener {
             if (a == "" )
-            {Toast.makeText(this@MainActivity3, "กรุณาเลือกคำตอบ", Toast.LENGTH_SHORT).show()
+            {
+                Toast.makeText(this@MainActivity4, "กรุณาเลือกคำตอบ", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            val i = Intent(this,MainActivity::class.java)
+            val i = Intent(this,MainActivity3::class.java)
             i.putExtra(extra,a.toString())
             startActivity(i)
     }
